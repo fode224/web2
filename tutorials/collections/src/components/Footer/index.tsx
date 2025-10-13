366255280fee9727c178e3d63cadd5dcc2f44b6e
fode224 <fode.bangura@student.vinci.be>
2025-10-13 14:58:41 +0200

    tuto5

---
+++ tutorials/collections/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import logo from "./assets/images/js-logo.jpg"
+
+export const Footer = () => {
+  return (
+    <footer>
+      <h1 className="animate__animated animate__bounce animate__delay-2s">
+        But we also love JS
+      </h1>
+      <img src={logo} alt="" />
+    </footer>
+  );
+};
