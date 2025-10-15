742d473c0c596d2f79244921a80833621863c819
fode224 <fode.bangura@student.vinci.be>
2025-10-15 16:25:49 +0200

    ex2.4

--- exercices/2.1-2-3-4/src/components/App.tsx
+++ exercices/2.1-2-3-4/src/components/App.tsx
@@ -1,5 +1,7 @@
 import { Movie } from "../types"
 import Cinema from "./Cinema"
+import Footer from "./Footer";
+import Header from "./Header";
 import PageTitle from "./PageTitle";
 
 const App = () => {
@@ -50,11 +52,20 @@ const moviesCinema2 :Movie[] = [
   
   return (
     <div>
-      <PageTitle title={pageTitle} />
+      <Header urlLgog="https://media.istockphoto.com/id/1429764305/fr/vectoriel/bande-de-film-vierge-isol%C3%A9e-sur-le-fond-blanc.jpg?s=1024x1024&w=is&k=20&c=is5Y6cun0NC8PxJd51p4YnUoLUpyb758Bdigh4Bqn48=">
+      <h1> Tous sur les films </h1>
+      </Header>
 
+      <main className="page-content">
+        <PageTitle title={pageTitle}/>
+  
       <Cinema name={cinema1Name} movies= {moviesCinema1} />
 
       <Cinema name={cinema2Name} movies={moviesCinema2} />
+      </main>
+      <Footer urlLogo="https://images.unsplash.com/photo-1760464864365-2188cd2afcde?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687">
+      <h1>je sais pas</h1>
+      </Footer>
     </div>
   );
 };
