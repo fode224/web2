8be1d3053ac0206796992e3da507bd1b2fddbfa1
fode224 <fode.bangura@student.vinci.be>
2025-10-20 15:13:33 +0200

    ex2.5-init

--- exercices/2.5/src/App.tsx
+++ exercices/2.5/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ClickCounter from './ClickCounter'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,11 +18,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>vite + react</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <ClickCounter/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
