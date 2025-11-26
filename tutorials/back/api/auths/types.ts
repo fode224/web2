f8a738fb828a462e8d9f89b49b7abc398a97812b
fode224 <fode.bangura@student.vinci.be>
2025-11-26 19:26:52 +0100

    ex3.1-doc

---
+++ tutorials/back/api/auths/types.ts
@@ -0,0 +1,64 @@
+import { Request } from "express";
+
+interface AuthenticatedRequest extends Request {
+  user?: User;
+}
+
+interface JwtPayload {
+  username: string;
+  exp: number; // Expiration time (in seconds since the epoch)
+  iat: number; // Issued at time (in seconds since the epoch)
+}
+
+
+interface Pizza {
+  id: number;
+  title: string;
+  content: string;
+}
+interface Drink {
+  id: number;
+  title: string;
+  image: string;
+  volume: number;
+  price: number;
+}
+
+interface PizzaToUpdate {
+  title?: string;
+  content?: string;
+}
+
+type NewPizza = Omit<Pizza, "id">;
+
+type NewDrink = Omit<Drink, "id">;
+
+interface AuthenticatedUser {
+  username: string;
+  token: string;
+}
+
+interface User {
+  id: number;
+  username: string;
+  password: string;
+}
+type PotentialUser = Omit<User, "id">;
+
+
+export type {
+  Pizza,
+  NewPizza,
+  PizzaToUpdate,
+  Drink,
+  NewDrink,
+  AuthenticatedUser,
+  User,
+   PotentialUser,
+   AuthenticatedRequest,
+   JwtPayload, 
+};
+
+
+
+
